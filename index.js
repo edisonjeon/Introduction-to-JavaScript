c895d50b616a554160cf1eb6a9ad4825c1e2b327
Edison Jeon <edison-jeon@lambdastudents.com>
2021-03-16 12:41:28 -0700

    finished, makes sense.

--- index.js
+++ index.js
@@ -265,8 +265,22 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
-  /*Your Code here */
+function grade(percent){
+  if (percent >= 90) {
+    return 'you got an A';
+  }
+  else if (percent >= 80) {
+    return 'you got a B';
+  }
+  else if (percent >= 70) {
+    return 'you got a C';
+  }
+  else if (percent >= 60) {
+    return 'you got a D';
+  } else {
+    return 'you got an F';
+  }
+  
   }
   
   
